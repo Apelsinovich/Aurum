3c8c18c28c41adae22f6797f13a8988e8affb33b
Apelsinovich <anejibcuh4uk@gmail.com>
2020-05-28 17:08:29 +0300

    Aurum_commit_3

--- app/src/main/java/com/example/myapplication/EventsActivity.kt
+++ app/src/main/java/com/example/myapplication/EventsActivity.kt
@@ -2,11 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class EventsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
+
+        val event1 = findViewById<TextView>(R.id.Event1)
+        event1.setText("Июнь, гп Северо-Енисейский, экологический квест Экобитва\nМесто проведение: МЦ Аурум")
+
+
+
     }
+
+
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<ImageView>(R.id.buttonEvent)
+        button.setOnClickListener{
+            val intent = Intent(this, EventsActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
