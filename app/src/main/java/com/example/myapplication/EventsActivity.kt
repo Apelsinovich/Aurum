350d547941e91b8b6101a03068961ecf4d94733f
Apelsinovich <anejibcuh4uk@gmail.com>
2020-05-29 15:14:40 +0300

    add_recycler_animals

--- app/src/main/java/com/example/myapplication/EventsActivity.kt
+++ app/src/main/java/com/example/myapplication/EventsActivity.kt
@@ -2,33 +2,92 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class EventsActivity : AppCompatActivity() {
 
+class EventsActivity : AppCompatActivity(), MyRecyclerViewAdapter.ItemClickListener {
+    private var adapter: MyRecyclerViewAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
-
+//кнопка назад
         val actionBar: ActionBar? = supportActionBar
         actionBar?.setHomeButtonEnabled(true)
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val event1 = findViewById<TextView>(R.id.Event1)
-        event1.setText("Июнь, гп Северо-Енисейский, экологический квест Экобитва\nМесто проведение: МЦ Аурум")
+
+        /*val event1 = findViewById<TextView>(R.id.Event1)
+        event1.setText("Июнь, гп Северо-Енисейский, экологический квест Экобитва\nМесто проведение: МЦ Аурум")*/
+
+        // data to populate the RecyclerView with
+
+        // data to populate the RecyclerView with
+        val animalNames: ArrayList<String> = ArrayList()
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+        animalNames.add("Horse")
+        animalNames.add("Cow")
+        animalNames.add("Camel")
+        animalNames.add("Sheep")
+        animalNames.add("Goat")
+
+        // set up the RecyclerView
+        val recyclerView: RecyclerView = findViewById(R.id.rvEvents)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MyRecyclerViewAdapter(this, animalNames)
+        adapter!!.setClickListener(this)
+        recyclerView.adapter = adapter
         
     }
-
+//для кнопки назад
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.getItemId()) {
+        return when (item.itemId) {
             android.R.id.home -> {
                 finish()
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+    override fun onItemClick(view: View?, position: Int) {
+        Toast.makeText(
+            this,
+            "You clicked " + adapter!!.getItem(position) + " on row number " + position,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
 }
