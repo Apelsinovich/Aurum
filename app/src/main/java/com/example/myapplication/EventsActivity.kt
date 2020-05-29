bebd4d6f38d2a85076b8ff968dad70d7f1c22075
Apelsinovich <anejibcuh4uk@gmail.com>
2020-05-29 14:45:56 +0300

    add_home_button

--- app/src/main/java/com/example/myapplication/EventsActivity.kt
+++ app/src/main/java/com/example/myapplication/EventsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 
 class EventsActivity : AppCompatActivity() {
 
@@ -10,13 +12,23 @@ class EventsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
 
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.setHomeButtonEnabled(true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
         val event1 = findViewById<TextView>(R.id.Event1)
         event1.setText("Июнь, гп Северо-Енисейский, экологический квест Экобитва\nМесто проведение: МЦ Аурум")
-
-
-
+        
     }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.getItemId()) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 }
